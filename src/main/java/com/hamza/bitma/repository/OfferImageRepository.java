7ea1d3a6abaa6df7f02b33cfa4b72152835ac235
devnart <hr.fb001@gmail.com>
2022-06-08 17:39:08 +0100

    a few edits here and there

--- src/main/java/com/hamza/bitma/repository/OfferImageRepository.java
+++ src/main/java/com/hamza/bitma/repository/OfferImageRepository.java
@@ -2,10 +2,11 @@ package com.hamza.bitma.repository;
 
 import com.hamza.bitma.entity.OfferImage;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import java.util.Arrays;
 import java.util.List;
-
+@Repository
 public interface OfferImageRepository extends JpaRepository<OfferImage,Long> {
     List<OfferImage> findAllByOfferId(Long offerId);
 }
